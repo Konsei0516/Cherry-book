846e4c0c90c46b9f6e3b2596209ea8d9297ae45e
Konsei0516 <masaya19960516@gmail.com>
2020-05-03 14:31:38 +0900

    minitest 記述

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,7 +10,17 @@ def fizz_buzz(n)
   end
 end
 
-fizz_buzz(1)
-fizz_buzz(5)
-fizz_buzz(3)
-fizz_buzz(15)
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'fizz', fizz_buzz(3)
+  end
+end
+
+# fizz_buzz(1)
+# fizz_buzz(5)
+# fizz_buzz(3)
+# fizz_buzz(15)
