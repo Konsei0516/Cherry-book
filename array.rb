c5d35145f0bcfacafbb824e892489fae24deada7
Konsei0516 <masaya19960516@gmail.com>
2020-05-06 23:46:46 +0900

    繰り返しメソッドの学習

---
+++ array.rb
@@ -0,0 +1,31 @@
+#times
+sum = 0
+5.times { |n| sum += n}
+sum
+
+#upto
+a = []
+10.upto(14) { |n| a << n}
+a
+
+# down
+a = []
+14.downto(10) { |n| a << n}
+a
+
+#step
+a = []
+1.step(10,2) { |n| a << n}
+a
+
+#while
+a = []
+while a.size < 5
+  a << 1
+end
+a
+
+#loop
+
+
+
