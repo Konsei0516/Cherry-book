02702ec9059916b768fcfcab3905d93ef52fd976
Konsei0516 <masaya19960516@gmail.com>
2020-05-18 19:30:28 +0900

    namespace学習
    classの重複を避ける

---
+++ namespace.rb
@@ -0,0 +1,16 @@
+module Baseball
+  class Second
+    def initialize(player,uniform_number)
+      @player = player
+      @uniform_number = uniform_number
+    end
+  end
+end
+
+module Clock
+  class Second
+    def initialize(digits)
+      @digits = digits
+    end
+  end
+end
